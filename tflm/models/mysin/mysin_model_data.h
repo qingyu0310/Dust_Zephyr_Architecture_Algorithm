99e5c15ff61b0bf13b6c456e21f39310dbedaa98
qingyu0620 <187825990+qingyu0620@users.noreply.github.com>
2026-08-01 21:39:51 +0800

    refactor: 架构层 Kconfig 符号统一 DUST_ 前缀

--- tflm/models/mysin/mysin_model_data.h
+++
@@ -1,2 +0,0 @@
-extern const unsigned char g_mysin_model_data[];
-extern const unsigned int g_mysin_model_data_size;
--- tflm/tensorflow/lite/kernels/TargetConditionals.h
+++
@@ -1,4 +0,0 @@
-#ifndef TARGET_CONDITIONALS_H_
-#define TARGET_CONDITIONALS_H_
-// Stub for Zephyr - original is Apple platform header
-#endif
